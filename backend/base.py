46bdb25f0f5dd9879cbb859e775c35bcb7720355
chris1234555 <Chris@Anyones-Macbook.wv.cc.cmu.edu>
2017-11-04 16:49:47 -0400

    Delete the shitty donation class

--- backend/base.py
+++ backend/base.py
@@ -12,13 +12,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     distance = db.Column(db.Integer, primary_key=True)
     school= db.Column(db.String(120), unique = True, nullable = False)
-    donation = db.relationship('donation', backref='user', lazy=True)
     def __repr__(self):
         return '<User %r>' % self.username
 
-
-class donation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    amount = db.Column(db.Integer, nullable=False)
-    person_id = db.Column(db.Integer, db.ForeignKey('user.id'),
-        nullable=False)
+ 
