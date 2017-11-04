9512f9d384089e50de09af1e8c5227384775ff8d
ChrisAReed <33382681+ChrisAReed@users.noreply.github.com>
2017-11-04 18:50:26 -0400

    Add files via upload
    Initial code for the laser detector

---
+++ wheelCode.ino
@@ -0,0 +1,16 @@
+int sensor = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue = analogRead(sensor);
+
+  Serial.println(sensorValue);
+  delay(10);
+  
+}
