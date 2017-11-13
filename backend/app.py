ae54cc9f3167fd0553f1bb4ec8e60c0dd1fee221
chris1234555 <Chris@Anyones-Macbook.wv.cc.cmu.edu>
2017-11-12 20:45:43 -0500

    Change flask

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask, render_template, request
 import stripe
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import stripe
+import ssl
 
 stripe_keys = {
   'secret_key': os.environ['SECRET_KEY'],
@@ -11,6 +15,24 @@ stripe.api_key = stripe_keys['secret_key']
 
 app = Flask(__name__)
 
+<<<<<<< Updated upstream
+=======
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    distance = db.Column(db.Integer, primary_key=True)
+    school= db.Column(db.String(120), unique = True, nullable = False)
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+>>>>>>> Stashed changes
 @app.route('/')
 def index():
     return render_template('index.html', key=stripe_keys['publishable_key'])
