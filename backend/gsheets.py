2061fc82de5663cd12c6feecd6be483067bcfa3d
Grant Seastream <gseastre@andrew.cmu.edu>
2017-11-13 20:15:49 -0500

    Added fetch function for Google sheet

--- backend/gsheets.py
+++ backend/gsheets.py
@@ -1,17 +1,25 @@
 import pygsheets
 
 
-def main():
-    gc = pygsheets.authorize('keys/client_secret.json', no_cache=True)
-
-    sh = gc.open_by_key('1VdSFlW9t2RS47tlfCfXQkzspByOuMVI3kFnx49XZlrU')
-
+def fetch_as_df():
+    gc = pygsheets.authorize('client_secret.json', no_cache=True)
+    sh = gc.open_by_key('1tTuWHk3c-JVvdOOSVVA1rIOjjz8eMf-Zyh1Fit5JwPc')
     wks = sh.worksheet_by_title('Data')
+    df = wks.get_as_df().set_index('Method')
+    return df
 
-    df = wks.get_as_df()
 
-    print(df)
+def fetch_gsheet_total():
+    df = fetch_as_df()
+    venmo_total = df.at['Venmo', 'Total']
+    cash_total = df.at['Cash', 'Total']
+    card_total = df.at['Card', 'Total']
+    # Expecting Stripe API to calculate card total itself
+    # Only report venmo and cash
+    # Probably will end up deleting the card option on the Google form
+    # Unless we implement donating towards certain brothers
+    return venmo_total + cash_total
 
 
 if __name__ == "__main__":
-    main()
+    print("Total money from Google Sheet: %d" % fetch_gsheet_total())
