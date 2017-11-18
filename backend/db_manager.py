355fd1829fffa9af6fb983fa40c076aebae9fe52
Grant Seastream <gseastre@andrew.cmu.edu>
2017-11-18 13:52:53 -0500

    Moved db classes to db_manager

--- backend/db_manager.py
+++ backend/db_manager.py
@@ -5,3 +5,21 @@ from flask import Flask
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+
+#Sensor Data Point
+class SensorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Integer, primary_key=True)
+    speed = db.Column(db.Integer, primary_key=True)
+
+
+#User database model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    distance = db.Column(db.Integer, primary_key=True)
+    school= db.Column(db.String(120), unique = True, nullable = False)
+    def __repr__(self):
+        return '<User %r>' % self.username
