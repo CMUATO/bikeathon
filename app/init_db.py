fa0b0826ce53028f31743d1cf5046c33daa6e40e
Grant Seastream <gseastre@andrew.cmu.edu>
2017-11-18 15:16:38 -0500

    Took out unique requirement for school

--- app/init_db.py
+++ app/init_db.py
@@ -5,11 +5,10 @@ from user import User
 db.create_all()
 
 
-# Currently fails if school is not unique
 users = [User(username='grant', school='cit'),
          User(username='cchris', school='hss'),
          User(username='brandon', school='scs'),
-         User(username='daniel', school='mcs')]
+         User(username='daniel', school='scs')]
 
 
 for user in users:
