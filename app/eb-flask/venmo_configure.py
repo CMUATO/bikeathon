485bd12e27b8e216d04eaa8c8546316e48f1ab14
Grant Seastream <gseastre@andrew.cmu.edu>
2017-11-28 01:16:38 -0500

    Added script to automatically update venmo token in config

---
+++ app/eb-flask/venmo_configure.py
@@ -0,0 +1,21 @@
+import venmo, json
+
+
+def config_venmo():
+    if not venmo.auth.configure():
+        print("Venmo configuration failed.")
+        return
+
+    access_token = venmo.auth.get_access_token()
+
+    config = open("config.json", "r").read()
+    configDict = json.loads(config)
+    configDict['venmo_token'] = access_token
+    configjson = json.dumps(configDict)
+    open("config.json", "w").write(configjson)
+
+    return access_token
+
+
+if __name__ == '__main__':
+    print(config_venmo())
