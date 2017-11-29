01a69eaf793370933e574d7596fad5e4b756c58b
Grant Seastream <gseastre@andrew.cmu.edu>
2017-11-28 23:11:22 -0500

    Move db url to config.json

--- app/eb-flask/db_manager.py
+++ app/eb-flask/db_manager.py
@@ -1,8 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 
+import json
+
+
+config = open("config.json").read()
+configDict = json.loads(config)
+db_url = configDict['db_url']
+
+
 #Set up SQL app
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///foo.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
