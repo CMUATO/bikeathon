2dc54b54568dcfd42f429b604eef8e17e9436e70
Grant Seastream <gseastre@andrew.cmu.edu>
2017-12-21 16:46:29 -0500

    Simplify time boundary math

--- app/eb-flask/static/main.js
+++ app/eb-flask/static/main.js
@@ -1,19 +1,13 @@
 "use strict";
 
 function updateTime(start, end) {
-  let elapsed = Math.min(Date.now(), end) - start;
+  let elapsed = Math.max(Math.min(Date.now(), end) - start, 0);
   let seconds = Math.floor(elapsed / 1000);
   let minutes = Math.floor(seconds / 60);
   let hours = Math.floor(minutes / 60);
 
   minutes = "0" + String(minutes % 60);
   seconds = "0" + String(seconds % 60);
-
-  if (elapsed < 0) {
-    minutes = "00";
-    seconds = "00";
-    hours = 0;
-  }
 
   let display = `${hours}:${minutes.slice(-2)}:${seconds.slice(-2)}`;
   $("#time").html(display);
