e4f3f071793381022350d7881a8d40c4ec60ea47
Grant Seastream <gseastre@andrew.cmu.edu>
2018-07-22 19:12:19 -0600

    Cleanup extra newlines

--- eb-flask/venmo_pull.py
+++ eb-flask/venmo_pull.py
@@ -1,6 +1,5 @@
 import venmo, requests, json
 
-
 def fetch_venmo_balance():
     config = open("config.json").read()
     configDict = json.loads(config)
@@ -15,7 +14,6 @@ def fetch_venmo_balance():
         print("Access token has expired. Please reauthorize by running "
               "venmo_configure.py. Returning None.")
         return None
-
 
 if __name__ == '__main__':
     print("Venmo balance: $%s" % fetch_venmo_balance())
