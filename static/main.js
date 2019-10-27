5d1fa868c549fa60dac172f7aa3cec7a0ffb0be6
jianminw <jianminw@andrew.cmu.edu>
2019-10-27 13:18:50 -0400

    finalize the start and end times

--- static/main.js
+++ static/main.js
@@ -14,8 +14,8 @@ function updateTime(start, end) {
 }
 
 function initTimer() {
-  let start = Date.parse("20 Oct 2019 8:00:00 EDT");
-  let end = Date.parse("21 Oct 2019 20:00:00 EDT");
+  let start = Date.parse("5 Nov 2019 8:00:00 EDT");
+  let end = Date.parse("6 Nov 2019 20:00:00 EDT");
   updateTime(start, end);
   let interval = setInterval(function () {
     updateTime(start, end);
