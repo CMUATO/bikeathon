9cff45c456702fd4b8c4a7861910bc6597f29a6c
wdrrwsunny <56174222+wdrrwsunny@users.noreply.github.com>
2021-01-20 15:52:14 -0700

    Added comments

--- datapusher.py
+++ datapusher.py
@@ -1,10 +1,12 @@
 import time, math, requests, json
 
+# Defining rider object used in other files #
 class Rider(object):
     def __init__(self, uid, wheel_radius, distance=0):
         """Wheel radius in inches"""
         self.uid = uid
 
+        # inches to miles #
         in_to_mil = 0.000015783
         radius = wheel_radius * in_to_mil
         self.circumference = 2 * math.pi * radius
@@ -17,6 +19,7 @@ class Rider(object):
         self.password = password
 
         self.distance = distance
+        # Payload is the update package to server #
         self.payload = dict(bikeid=self.uid,
                             distance=self.distance,
                             password=self.password)
@@ -34,18 +37,25 @@ class Rider(object):
             configjson = file.read()
 
         configDict = json.loads(configjson)
+        # Update local json with the up to date distance info #
+        # I think this code is implicitly assuming there is one rider #
+        # To add more riders, we need to be careful with editting dictionary #
         configDict["distance"] = self.distance
         configjson = json.dumps(configDict, indent=4, sort_keys=True)
 
+        # Commit changes #
         with open("piconfig.json", "w") as file:
             file.write(configjson)
 
         print("Pushing...")
         headers = {"content-type": "application/json"}
+        
+        # Push commend on the side of Raspberry Pi #
         r = requests.post(self.url, data=json.dumps(self.payload),
             headers=headers, timeout=3)
         print(r)
 
+    # Changer function used in sensor.py #
     def Changer(self, push):
         """Add distance to payload"""
         self.Update_Payload()
